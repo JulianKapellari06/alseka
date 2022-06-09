9ba803bd7e6a7c3b265caeca148b9a521f331e13
mxhxmmxd <mohammed.alamer@edu.fh-joanneum.at>
2022-06-09 14:19:48 +0200

    added javaDoc comments to DoubleQueue

--- src/main/java/at/fhj/msd/DoubleQueue.java
+++ src/main/java/at/fhj/msd/DoubleQueue.java
@@ -4,14 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+/**
+ * DrinksQueue represents a double list
+ * @author Marin Sekic
+ */
 public class DoubleQueue implements IQueue{
+
+    /**
+     *  the menu and its size/length
+     */
     private List<Double> elements = new ArrayList<Double>();
     private int maxSize;
 
+
+    /**
+     * DoubleQueue(int maxSize): constructor of the Double list
+     * @param maxSize determinants the amount of drinks in the list
+     */
     public DoubleQueue(int size){
         this.maxSize = size;
     }
 
+
+    /**
+     * adds a Double to the menu
+     * @param obj is data type Object, in order to add different types of data to the list
+     * @return true or false, true if a double could be added successfully
+     * @author Marin Sekic
+     */
     @Override
     public boolean offer(Object obj) {
         if (elements.size() != maxSize)
@@ -22,6 +42,12 @@ public class DoubleQueue implements IQueue{
         return true;
     }
 
+    /**
+     * gets the first double in the menu and then removes it from menu/list
+     * @return null if there is no double in the list, the first double as element if there is
+     * any doubles in list
+     * @author Marin Sekic
+     */
     @Override
     public Object poll() {
         Double element = (Double) peek();
@@ -33,6 +59,12 @@ public class DoubleQueue implements IQueue{
         }
     }
 
+    /**
+     * gets the first double in the menu and then removes it from menu/list
+     * @return NoSuchElementException if there is no double in the list, the first double as element if there is
+     * any doubles in list
+     * @author Marin Sekic
+     */
     @Override
     public Object remove() {
         Double element = (Double) peek();
@@ -41,6 +73,13 @@ public class DoubleQueue implements IQueue{
         elements.remove(element);
         return element;
     }
+
+    /**
+     * gets the first double in the menu and then removes it from list
+     * @return Null if there is no double in the list, the first double as element if there is
+     * any doubles in list
+     * @author Marin Sekic
+     */
 
     @Override
     public Object peek() {
@@ -52,6 +91,13 @@ public class DoubleQueue implements IQueue{
 
         return element;
     }
+
+    /**
+     * gets the first double in the menu and then removes it from list
+     * @return NoSuchElementException if there is no double in the list, the first double as element if there is
+     * any doubles in list
+     * @author Marin Sekic
+     */
 
     @Override
     public Object element() {

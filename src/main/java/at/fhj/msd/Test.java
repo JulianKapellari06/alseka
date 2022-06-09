555331f4560b99df90181e7ba6adcb293262a333
Marin Sekic <marin.sekic@edu.fh-joanneum.at>
2022-06-09 10:37:52 +0200

    remove class implemented

--- src/main/java/at/fhj/msd/Test.java
+++ src/main/java/at/fhj/msd/Test.java
@@ -7,5 +7,10 @@ public class Test {
         Liquid liquid = new Liquid("Long Island Icetea", 2, 2);
         System.out.println(queue.offer(new SimpleDrink("sbkjd", liquid)));
 
+        System.out.println(queue.offer(new SimpleDrink("sbkjd2", liquid)));
+        System.out.println(queue.remove());
+        System.out.println(queue.offer(new SimpleDrink("sbkjd3", liquid)));
+
+        System.out.println(queue.poll());
     }
 }
